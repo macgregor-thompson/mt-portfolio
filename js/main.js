d46b4ceba7c3fdf4c305a19e3b87f483558579a1
MacGregor Thompson <mhthompson86@gmail.com>
2018-10-16 15:43:24 -0400

    - create contact form to email me instead of relying on the mailto crap - contact form uses the azure node function that sends emails to me and one to the user - add in ability to use a spinner for when it's waiting on the server response, but don't feel like messing with it just yet...

--- js/main.js
+++ js/main.js
@@ -1,22 +1,72 @@
-(function (keyframes, options) {
+(function () {
   'use strict';
 
+  let contactForm = $('#contact-form').parsley();
+  let data = {
+    name: '',
+    email: '',
+    message: ''
+  };
+
+  listeners();
   scrollToRelativeLinks();
   toggleBackToTop();
   fixIEScrollCrap();
+
+
+  function listeners() {
+    $('.disable-link').on('click', function () {
+      return false;
+    });
+
+    $('#send-message').on('click', function () {
+      contactForm.validate();
+      const url = 'https://mt-web-portfolio-emailer.azurewebsites.net/api/Emailer?code=Q4N0nc7gR1ARWJhC9CU5kDYEkUYhWuPjFohbjavI6jExr8BQ83pWYg==';
+      if (contactForm.isValid()) {
+        data.name = $('#name').val();
+        data.email = $('#email').val();
+        data.message = $('#message').val();
+        $.post(url, JSON.stringify(data))
+          .done(resp => {
+            // $('#contact-modal').modal('hide');
+            // $('#thank-you-modal').modal('show');
+            // $('#message-error').hide();
+            // $('#contact-form')[0].reset();
+            // contactForm.reset();
+            console.log('response:', resp);
+          })
+          .fail(e => {
+            // $('#thank-you-modal').modal('hide');
+            // $('#message-error').show();
+            console.log('Error:', e);
+          });
+        $('#contact-modal').modal('hide');
+        $('#contact-form')[0].reset();
+        contactForm.reset();
+      }
+    });
+  }
+
+
+  function formToJSON(elements) {
+    return [].reduce.call(elements, (data, element) => {
+      data[element.name] = element.value;
+      return data;
+    }, {});
+  }
 
 
   function scrollToRelativeLinks() {
     $('a').on('click', function (e) {
       console.log('link clicked:', e);
       if (e.target.hash.indexOf('#') === 0) {
-        var pos = $(e.target.hash).offset().top;
+        let pos = $(e.target.hash).offset().top;
         console.log('relative link, pos:', pos);
         $('html, body').animate({
             scrollTop: pos - 50
           }, pos / 5,    // this is the animation time
           function () {
-          console.log('returning false');
+            console.log('returning false');
             return false;
           }    // what to do after the animation
         );
@@ -35,14 +85,14 @@
   }
 
   function fixIEScrollCrap() {
-    if(navigator.userAgent.match(/Trident\/7\./)) { // if IE
+    if (navigator.userAgent.match(/Trident\/7\./)) { // if IE
       $('body').on("mousewheel", function () {
         // remove default behavior
         event.preventDefault();
 
         //scroll without smoothing
-        var wheelDelta = event.wheelDelta;
-        var currentScrollPosition = window.pageYOffset;
+        let wheelDelta = event.wheelDelta;
+        let currentScrollPosition = window.pageYOffset;
         window.scrollTo(0, currentScrollPosition - wheelDelta);
       });
     }
